144dae6a4bf7b1dedff72880778b216b1eacbb1a
Alessandro Maggi <maggialessandro360@gmail.com>
2023-06-10 18:59:56 +0200

    feat: page layout

--- src/lib/Content.svelte
+++ src/lib/Content.svelte
@@ -1,3 +1,16 @@
-<div>
-  <p class="text-6xl text-gray-200">Python Verona</p>
+<div class="flex w-full items-center justify-center">
+  <div class="h-fit w-4/6 -translate-y-1/3">
+    <h1 class="text-6xl text-gray-200">Python Verona</h1>
+    <div
+      class="mt-8 grid h-40 grid-cols-2 gap-6 justify-self-stretch text-3xl text-gray-200"
+    >
+      {#each Array(4) as i}
+        <div
+          class="flex items-center rounded-3xl border-2 border-gray-400 bg-[#4C67C8] duration-200 hover:-translate-y-1 hover:bg-[#5b7cf2] hover:drop-shadow-lg"
+        >
+          <p class="ml-6">Lorem</p>
+        </div>
+      {/each}
+    </div>
+  </div>
 </div>
